58c532dd2632dea862d1e6d9ca2c05882f34213d
raja00912cd <mdramiz025@gmail.com>
2022-11-28 20:14:17 +0530

    updated login page

---
+++ ajio_dublicate/src/login-component/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+
+    return (
+        <div>
+            <input placeholder="Enter Email" />
+            <input placeholder="Enter Password" />
+            <input type="submit" />
+        </div>
+    )
+}
