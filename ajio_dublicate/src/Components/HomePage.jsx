8b6718068f45a4dd399569b7d79bed00fd5a1498
Shruti Joshi <shrutijoshi@Shrutis-MacBook-Pro.local>
2022-12-01 14:50:37 +0530

    homePage progress

--- ajio_dublicate/src/Components/HomePage.jsx
+++ ajio_dublicate/src/Components/HomePage.jsx
@@ -18,6 +18,27 @@ const images2 = [
    "https://assets.ajio.com/cms/AJIO/WEB/D-RelianceOne-Bankoffer.jpg"
 ]
 
+const images3 = [
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-p1-bestofwinter-upto75.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-westernwear.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-ethnicwear.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-footwear.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-Kidswear.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-Home&Kitchen.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-Acessories.jpg"
+    
+]
+
+const images4 = [
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p2-vanheusen-min40.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p3-andglobaldesi-brands-min60.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p4-weddingcollection-brands-min65.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p5-leewrangler-brands-min50.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p5-leewrangler-brands-min50.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p6-westerndresses-brands-starting499.jpg",
+    "https://assets.ajio.com/cms/AJIO/WEB/1122022-d-uhp-JIT-p7-innerwear-brands-starting199.jpg"
+    
+]
 const responsive = {
     responsiveClass: true,
     loop:true,
@@ -75,6 +96,79 @@ export const HomePage = () => {
             <div className="bag_section_banner" style = {{marginTop: "10px", cursor: "pointer"}}>
                 <img src="https://assets.ajio.com/cms/AJIO/WEB/D-opt-1.gif" width= "1440px"height = "90" alt="" />
             </div>
+            <div className="brands_section_banner">
+                <img src="https://assets.ajio.com/cms/AJIO/WEB/D-SponsorBrands-Header.jpg" alt="" />
+
+            </div>
+            <div className="brands_section" style = {{display: "flex", animation: "ease-out", cursor: "pointer"}}>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-SponsorBrands-p1-levis.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-SponsorBrands-p2-adidas.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-SponsorBrands-p3-campus.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-SponsorBrands-p4-puma.jpg" alt="" /></div>
+            </div>
+            <div>
+                <img src="https://assets.ajio.com/cms/AJIO/WEB/D-Bestsellers-Header.jpg" alt="" />
+            </div>
+            <Box width="100%" margin="auto" cursor="pointer" >
+            <Box width="100%" >
+            <OwlCarousel {...responsive} className="owl-theme" >
+            {images3.map((img)=>(
+                <Box key={img} className='hover-underline-banner' width="100%" margin="auto" mt={10} >
+                    <Image w="1468px"  height= "450px" marginTop= {{base: "-45px"}} src={img} />
+                </Box>
+            ))}
+            </OwlCarousel>
+        </Box>
+        
+    </Box>
+        <div className="ajio_exclusive_section" style = {{marginTop: "4px"}}>
+            <img src="https://assets.ajio.com/cms/AJIO/WEB/D-ExclusivelyonAJIO-Header.jpg" alt="" />
+        </div>
+        <div className="exclusive_section" style = {{display: "flex", animation: "ease-out", cursor: "pointer"}}>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-ExclusivelyAJIO-p1-Exclusiverange-REVISED%20(1).jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-ExclusivelyonAJIO-p2-ProduktbyJ&J.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-ExclusivelyonAJIO-p3-Avaasa.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-ExclusivelyonAJIO-p4-Gas.jpg" alt = "" /></div>
+            </div>
+        <div className="shineStyle_banner">
+                <div><img src="https://assets.ajio.com/cms/AJIO/WEB/D-JIT-Header.jpg" alt="" /></div>
+
+        </div>
+
+        <Box width="100%" margin="auto" cursor="pointer" >
+            <Box width="100%" >
+            <OwlCarousel {...responsive} className="owl-theme" >
+            {images4.map((img)=>(
+                <Box key={img} className='hover-underline-banner' width="100%" margin="auto" mt={10} >
+                    <Image w="1468px"  height= "450px" marginTop= {{base: "-45px"}} src={img} />
+                </Box>
+            ))}
+            </OwlCarousel>
+        </Box>
+        
+    </Box>
+    <div className="ajioService_banner">
+        <img src="https://assets.ajio.com/cms/AJIO/WEB/D-AJioservice-header.jpg" alt="" />
+    </div>
+    <div>
+        <img src="https://assets.ajio.com/cms/AJIO/WEB/D-AJioservice-Nextday.jpg" alt="" />
+    </div>
+    <div>
+        <img src="https://assets.ajio.com/cms/AJIO/WEB/D-Latestlaunches-Header.jpg" alt="" />
+    </div>
+    <div className="latest_launches">
+        <img src="https://assets.ajio.com/cms/AJIO/WEB/2022_GAP_AJIO_Cozy%20Club_Desktop_w1440px%20X%20h470px_Unisex.jpg" alt="" />
+    </div>
+
+    <div className = "latestLauch_section" style = {{display: "flex", overflow: "auto", whiteSpace: "nowrap"}}>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-LatestLaunches-lancer-neqw.jpg"  alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-Latestlaunches-p3-truereligion-new.jpg" alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-LatestLaunches-710-new.jpg"  alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-LatestLaunches-KendallKylie.jpg"  alt = "" /></div>
+                {/* <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-Latestlaunches-p1-Acai-new.jpg"  alt="" /></div>
+                <div style = {{marginRight: "0.5px"}}><img src="https://assets.ajio.com/cms/AJIO/WEB/D-Latestlaunches-p2-FyreRose-new.jpg"  alt="" /></div> */}
+               
+    </div>
 </div>
   )
 }
