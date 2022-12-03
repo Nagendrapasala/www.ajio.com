3b5524377d7488623a50c833bf1cce2aea28ff37
Sheikhzain07 <sheikhzain459@gmail.com>
2022-12-04 01:47:00 +0530

    contact us done

--- ajio_dublicate/src/App.js
+++ ajio_dublicate/src/App.js
@@ -1,14 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ProductDetails from "./ProductDetails/ProductDetails";
+import Contact from "./Contact/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <ProductDetails />
-      {/* <ButtonComp /> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
