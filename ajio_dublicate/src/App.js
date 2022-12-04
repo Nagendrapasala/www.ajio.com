69ac4767f2e8a0423afa14d8bf454de259db94fb
Shruti Joshi <shrutijoshi@Shrutis-MacBook-Pro.local>
2022-12-04 19:19:50 +0530

    done

--- ajio_dublicate/src/App.js
+++ ajio_dublicate/src/App.js
@@ -1,35 +1,11 @@
 
 
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer'
-import { HomePage } from './Components/HomePage';
-import Contact from './Components/Contact';
-import {Routes, Route} from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react';
+
 
 
 function App() {
   return (
     <div className="App">
-     
-        {/* <Navbar/> 
-        
-          
-       
-        
-        <Routes>
-          
-          <Route path = {`/productPage/${Men}`} element = {<ProductPage/>}/>
-          <Route path = "/women" element = {<h1>WOMEN</h1>}/>
-          <Route path = "/kids" element = {<h1></h1>}/>
-          <Route path = "/" element = {<HomePage/>}/>
-          <Route path = "/contact" element = {<Contact/>}/>
-        </Routes>
-        
-        <Footer/> */}
-        
-       
-        
     </div>
   );
 }
