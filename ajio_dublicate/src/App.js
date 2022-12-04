06175c266a1e447574e801a8d9d9ba337e1569d3
Nagendrapasala <31000696+Nagendrapasala@users.noreply.github.com>
2022-12-04 20:22:43 +0530

    Update App.js

--- ajio_dublicate/src/App.js
+++ ajio_dublicate/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+
 import { ChakraProvider } from "@chakra-ui/react";
 import {Productpage} from "./product_page/productpage.js"
 function App() {
   return (
     
     <div className="App">
+    
       <ChakraProvider>
       <Productpage />
       </ChakraProvider>
