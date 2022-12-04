426bea12c24e11891a301b6c13e37e276a05f58b
Shruti Joshi <shrutijoshi@Shrutis-MacBook-Pro.local>
2022-12-04 20:01:51 +0530

    latest

--- ajio_dublicate/src/App.js
+++ ajio_dublicate/src/App.js
@@ -1,11 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
 
-
-
-
-
+import { ChakraProvider } from "@chakra-ui/react";
+import {Productpage} from "./product_page/productpage.js"
 function App() {
   return (
     <div className="App">
+    
+      <ChakraProvider>
+      <Productpage />
+      </ChakraProvider>
     </div>
   );
 }
